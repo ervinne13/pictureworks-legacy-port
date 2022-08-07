daf5672db695d5c18d43d38eadbbae15a352be0d
Ervinne Sodusta <ervinnesodusta@gmail.com>
2022-08-07 23:21:03 +0800

    Remove unused routes

--- pictureworks-server/routes/api.php
+++ pictureworks-server/routes/api.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('v1')->group(function () {
     Route::middleware('auth.statickey')->group(function () {
         Route::resource('users.comments', UserCommentController::class)->only([
